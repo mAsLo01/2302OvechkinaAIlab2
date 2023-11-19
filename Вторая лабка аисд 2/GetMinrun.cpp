69b186d2365592acd7d2295e145f3dd607ec2971
mAsLo01 <133914695+mAsLo01@users.noreply.github.com>
2023-11-19 18:53:04 +0300

    Delete Вторая лабка аисд 2

--- Вторая лабка аисд 2/GetMinrun.cpp
+++
@@ -1,9 +0,0 @@
-int GetMinrun(int len)
-{
-	int r = 0;
-	while (len >= 64) {
-		r |= len & 1;
-		len >>= 1;
-	}
-	return len + r;
-}
--- Вторая лабка аисд 2/swap.cpp
+++
@@ -1,7 +0,0 @@
-#include"funct.h"
-void swap(int* a, int* b)
-{
-	int s = *a;
-	*a = *b;
-	*b = s;
-}
